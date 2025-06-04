b1f3304516745eed20d5c3a30554ae8592e8b5b3
MarioGranada <mario.hernandez@yolo.com>
2025-06-04 12:08:21 +0300

    flights and vehicles examples done

--- lib/live_view_studio_web/live/sales_live.ex
+++ lib/live_view_studio_web/live/sales_live.ex
@@ -4,14 +4,24 @@ defmodule LiveViewStudioWeb.SalesLive do
   alias LiveViewStudio.Sales
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        new_orders: Sales.new_orders(),
-        sales_amount: Sales.sales_amount(),
-        satisfaction: Sales.satisfaction()
-      )
+    # socket =
+    #   assign(socket,
+    #     new_orders: Sales.new_orders(),
+    #     sales_amount: Sales.sales_amount(),
+    #     satisfaction: Sales.satisfaction()
+    #   )
 
-    {:ok, socket}
+    # {:ok, socket}
+
+    if connected?(socket) do
+      # ******** Here -> NEED TO CHECK IF THE SOCKET IS CONNECTED
+      # ***************************************************** VVV
+      # why?, because mount is called twice: 1 for the first http request that comes to the browser and another one when the socket is connected.
+      # We want to manipulate data once we have a stateful process (which is created when the socket connection is created and stable)
+      :timer.send_interval(1000, self(), :tick)
+    end
+
+    {:ok, assign_stats(socket)}
   end
 
   def render(assigns) do
@@ -45,10 +55,26 @@ defmodule LiveViewStudioWeb.SalesLive do
         </div>
       </div>
 
-      <button>
+      <button phx-click="refresh">
         <img src="/images/refresh.svg" /> Refresh
       </button>
     </div>
     """
+  end
+
+  def handle_event("refresh", _params, socket) do
+    {:noreply, assign_stats(socket)}
+  end
+
+  def handle_info(:tick, socket) do
+    {:noreply, assign_stats(socket)}
+  end
+
+  defp assign_stats(socket) do
+    assign(socket,
+      new_orders: Sales.new_orders(),
+      sales_amount: Sales.sales_amount(),
+      satisfaction: Sales.satisfaction()
+    )
   end
 end
--- lib/live_view_studio_web/live/vehicles_live.ex
+++ lib/live_view_studio_web/live/vehicles_live.ex
@@ -1,10 +1,13 @@
 defmodule LiveViewStudioWeb.VehiclesLive do
   use LiveViewStudioWeb, :live_view
+  alias LiveViewStudio.Vehicles
 
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
-        vehicles: []
+        vehicles: [],
+        make_model: "",
+        loading: false
       )
 
     {:ok, socket}
@@ -14,20 +17,25 @@ defmodule LiveViewStudioWeb.VehiclesLive do
     ~H"""
     <h1>🚙 Find a Vehicle 🚘</h1>
     <div id="vehicles">
-      <form>
+      <form phx-submit="search">
         <input
           type="text"
           name="query"
-          value=""
+          value={@make_model}
           placeholder="Make or model"
           autofocus
           autocomplete="off"
+          readonly={@loading}
         />
 
         <button>
           <img src="/images/search.svg" />
         </button>
       </form>
+
+      <div :if={@loading} class="loader">
+        Loading...
+      </div>
 
       <div class="vehicles">
         <ul>
@@ -46,5 +54,24 @@ defmodule LiveViewStudioWeb.VehiclesLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("search", %{"query" => make_model}, socket) do
+    send(self(), {:run_search, make_model})
+
+    socket = assign(socket, make_model: make_model, vehicles: [], loading: true)
+
+    {:noreply, socket}
+  end
+
+  def handle_info({:run_search, make_model}, socket) do
+    socket =
+      assign(socket,
+        make_model: make_model,
+        vehicles: Vehicles.search(make_model),
+        loading: false
+      )
+
+    {:noreply, socket}
   end
 end
