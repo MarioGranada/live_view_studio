d0986a7584ffc307886f61f69dec8bfb137a4777
MarioGranada <mario.hernandez@yolo.com>
2025-06-26 13:59:14 +0300

    add donations pagination

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -20,14 +20,23 @@ defmodule LiveViewStudioWeb.DonationsLive do
     sort_by = valid_sort_by(params)
     sort_order = valid_sort_order(params)
 
+    # page = (params["page"] || "1") |> String.to_integer()
+    # per_page = (params["per_page"] || "5") |> String.to_integer()
+    page = param_to_integer(params["page"], 1)
+    per_page = param_to_integer(params["per_page"], 5)
+    donation_count = Donations.count_donations()
+
     options = %{
       sort_by: sort_by,
-      sort_order: sort_order
+      sort_order: sort_order,
+      page: page,
+      per_page: per_page
     }
 
     donations = Donations.list_donations(options)
 
-    socket = assign(socket, donations: donations, options: options)
+    socket =
+      assign(socket, donations: donations, options: options, donation_count: donation_count)
 
     {:noreply, socket}
   end
@@ -37,11 +46,25 @@ defmodule LiveViewStudioWeb.DonationsLive do
   slot :inner_block, required: true
 
   def sort_link(assigns) do
+    params = %{
+      assigns.options
+      | sort_by: assigns.sort_by,
+        sort_order: next_sort_order(assigns.options.sort_order)
+    }
+
+    assigns = assign(assigns, params: params)
+
     ~H"""
-    <.link patch={~p"/donations?#{%{sort_by: @sort_by, sort_order: next_sort_order( @options.sort_order)}}"}>
+    <.link patch={~p"/donations?#{@params}"}>
     <%= render_slot(@inner_block) %> <span><%= sort_indicator(@sort_by, @options) %></span>
     </.link>
     """
+  end
+
+  def handle_event("select-per-page", %{"per-page" => per_page}, socket) do
+    params = %{socket.assigns.options | per_page: per_page}
+    socket = push_patch(socket, to: ~p"/donations?#{params}")
+    {:noreply, socket}
   end
 
   defp next_sort_order(sort_order) do
@@ -75,4 +98,32 @@ defmodule LiveViewStudioWeb.DonationsLive do
   end
 
   defp valid_sort_order(_params), do: :asc
+
+  defp param_to_integer(nil, default), do: default
+
+  defp param_to_integer(param, default) do
+    case Integer.parse(param) do
+      {number, _} ->
+        number
+
+      :error ->
+        default
+    end
+  end
+
+  defp more_pages?(options, donation_count) do
+    options.page * options.per_page < donation_count
+  end
+
+  defp pages(options, donation_count) do
+    page_count = ceil(donation_count / options.per_page)
+
+    for page_number <- (options.page - 2)..(options.page + 2),
+        page_number > 0 do
+      if page_number <= page_count do
+        current_page? = page_number == options.page
+        {page_number, current_page?}
+      end
+    end
+  end
 end
